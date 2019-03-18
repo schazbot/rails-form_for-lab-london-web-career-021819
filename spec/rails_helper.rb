275bb70b810e0948a2a2f1ea65316132658ec3fd
github username <github email address>
2019-03-18 18:20:17 +0000

    Done.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
+ENV["RAILS_ENV"] ||= 'development'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
@@ -46,6 +46,6 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
-  
+
   config.infer_spec_type_from_file_location!
 end
